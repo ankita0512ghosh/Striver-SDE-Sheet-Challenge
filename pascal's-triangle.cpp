f8b6e7ca631eb2e04152a79ca297f245e5cf69f7
ankita0512ghosh <ankita0512ghosh@gmail.com>
2023-06-07 11:46:26 +0530

    Day 2 07/06/2023

---
+++ pascal's-triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+vector < vector < long long int >> printPascal(int n) {
+    vector < vector < long long int >> triangle;
+    vector < long long int > temp;
+
+    for (int i = 1; i <= n; i++) {
+        long long int rCe = 1;
+        temp.clear();
+        for (int j = 1; j <= i; j++) {
+            temp.push_back(rCe);
+            rCe = rCe * (i - j) / j;
+        }
+
+        triangle.push_back(temp);
+    }
+    return triangle;
+}
